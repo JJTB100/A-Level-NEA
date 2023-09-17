bccd2b16f0f569bff5deeda241fabec387e06141
Jonathan Broster <jonathan@broster.co.uk>
2023-09-17 12:08:11 +0100

    Testing

--- PianoChordsGame/PianoChordsGame/Form1.Designer.cs
+++ PianoChordsGame/PianoChordsGame/Form1.Designer.cs
@@ -28,12 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pbVolume = new ProgressBar();
+            comboBox1 = new ComboBox();
+            btnStart = new Button();
+            SuspendLayout();
+            // 
+            // pbVolume
+            // 
+            pbVolume.Location = new Point(522, 12);
+            pbVolume.Name = "pbVolume";
+            pbVolume.Size = new Size(266, 23);
+            pbVolume.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(275, 12);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 2;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnStart);
+            Controls.Add(comboBox1);
+            Controls.Add(pbVolume);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ProgressBar pbVolume;
+        private ComboBox comboBox1;
+        private Button btnStart;
     }
 }
--- PianoChordsGame/PianoChordsGame/Form1.cs
+++ PianoChordsGame/PianoChordsGame/Form1.cs
@@ -1,3 +1,5 @@
+using NAudio.Wave;
+
 namespace PianoChordsGame
 {
     public partial class Form1 : Form
@@ -5,6 +7,45 @@ namespace PianoChordsGame
         public Form1()
         {
             InitializeComponent();
+            for (int i = -1; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+            {
+                var caps = NAudio.Wave.WaveIn.GetCapabilities(i);
+                comboBox1.Items.Add($"{i}: {caps.ProductName}");
+            }
+
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var waveIn = new NAudio.Wave.WaveInEvent
+            {
+                DeviceNumber = comboBox1.SelectedIndex, // indicates which microphone to use
+                WaveFormat = new NAudio.Wave.WaveFormat(rate: 44100, bits: 16, channels: 1),
+                BufferMilliseconds = 20
+            };
+
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+            waveIn.StartRecording();
+        }
+        void WaveIn_DataAvailable(object? sender, NAudio.Wave.WaveInEventArgs e)
+        {
+            // copy buffer into an array of integers
+            Int16[] values = new Int16[e.Buffer.Length / 2];
+            Buffer.BlockCopy(e.Buffer, 0, values, 0, e.Buffer.Length);
+
+            Console.WriteLine(e.Buffer.ToString());
+            float fraction = (float)(values.Max() / 32768 * 100);
+            int percent = (int)Math.Ceiling(fraction);
+
+            //output to volume meter
+            pbVolume.Value = percent;
+            string bar = new('#', (int)(fraction * 70));
+            string meter = "[" + bar.PadRight(60, '-') + "]";
+            Console.CursorLeft = 0;
+            Console.CursorVisible = false;
+            Console.Write($"{meter} {fraction * 100:00.0}%");
         }
     }
+
 }
